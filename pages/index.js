486e01b47280edeaff76533f0f2d420112daac49
Rishi Mohan <iamrishi.ms@gmail.com>
2021-11-14 23:32:00 +0530

    Added open source

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <article className="w-full px-10 py-10 overflow-y-auto max-w-[620px] mx-auto">
+      <article className="w-full px-10 pt-10 pb-20 overflow-y-auto max-w-[620px] mx-auto">
         <div className="flex items-start justify-center w-full mb-10 overflow-hidden rounded-lg max-h-[500px] shadow-xl">
           <img
             src="/images/pages/rishi-mohan.jpg"
@@ -12,19 +12,40 @@ export default function Home() {
             title="A black and white photo of Rishi"
           />
         </div>
-        <h2 className="mt-12 mb-6 text-3xl font-black md:text-5xl"><span className="text-gray-400">Hi, I'm </span>Rishi Mohan!</h2>
+        <h2 className="mt-12 mb-6 text-3xl font-black md:text-5xl">
+          <span className="text-gray-400">Hi, I'm </span>Rishi Mohan!
+        </h2>
         <div className="post-content">
           <p className="text-lg text-gray-500">
             I'm a <span className="font-medium text-black">designer</span> and{" "}
-            <span className="font-medium text-black">front-end engineer</span> by
-            profession, currently working at <a href="https://www.bigbinary.com">BigBinary</a>.
+            <span className="font-medium text-black">front-end engineer</span>{" "}
+            by profession, currently working at{" "}
+            <a href="https://www.bigbinary.com">BigBinary</a>.
           </p>
           <p className="text-lg text-gray-500">
-            I like building clean and interactive apps using React.js, React Native and other front-end tech.
+            I like building clean and interactive apps using React.js, React
+            Native and other front-end tech.
           </p>
           <p className="text-lg text-gray-500">
-            On weekends I like to travel and take photos. I like to binge try Cafes and Restaurants. Some weekends go into building some side projects like <Link href="/work/kizie"><a>Kizie</a></Link> and <Link href="/work/maazi"><a>Maazi</a></Link>.
+            On weekends I like to travel and take photos. I like to binge try
+            Cafes and Restaurants. Some weekends go into building some side
+            projects like{" "}
+            <Link href="/work/kizie">
+              <a>Kizie</a>
+            </Link>{" "}
+            and{" "}
+            <Link href="/work/maazi">
+              <a>Maazi</a>
+            </Link>
+            .
           </p>
+        </div>
+        <div className="px-4 py-3 my-5 text-base text-gray-400 bg-gray-100 rounded-xl">
+          The code for this blog is{" "}
+          <a className="underline" target="_blank" href="https://github.com/rishimohan/rishimohan.me">
+            available on Github
+          </a>
+          .
         </div>
       </article>
     </>
