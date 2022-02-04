03e2f31640ded9cfd00613fef28efb222b3f8b0d
Rishi Mohan <iamrishi.ms@gmail.com>
2022-02-04 13:04:13 +0530

    Added experiments

--- pages/experiments/index.js
+++ pages/experiments/index.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react';
 import Link from "next/link"
 import { ArrowIcon } from "lib/icons";
 import classnames from "classnames";
+import {NextSeo} from "next-seo";
 
 export default function Experiments() {
   const [animate, setAnimate] = useState(false);
@@ -27,70 +28,88 @@ export default function Experiments() {
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden antialiased bg-white dark:bg-black md:p-10">
-      <div
-        className={classnames(
-          "absolute inset-0 flex items-center justify-center w-full h-full"
-        )}
-      >
+    <>
+      <NextSeo
+        title="Design & Code Explorations - Rishi Mohan"
+        description="A collection of design and code experiments, a college where student are ideas. Would the ideas graduate? Let's find out!"
+        openGraph={{
+          site_name: "Hi, I'm Rishi Mohan!",
+          title: "Design and Code Explorations - Rishi Mohan",
+          description:
+            "A collection of design and code experiments, a college where student are ideas. Would the ideas graduate? Let's find out!",
+        }}
+      />
+      <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden antialiased bg-white dark:bg-black">
         <div
           className={classnames(
-            "w-full h-full duration-1000 bg-gradient-to-br from-indigo-500 dark:from-indigo-600/70 dark:via-red-700/40 dark:to-gray-700/40 via-red-500/40 to-red-500 blur-[250px] transform-gpu",
-            { "scale-[0.65] dark:opacity-70 rotate-[10deg]": animate },
-            { "scale-0 opacity-0 animate-pulse rotate-0": !animate }
+            "absolute inset-0 flex items-center justify-center w-full h-full overflow-hidden"
           )}
-        />
-        <div
-          className="absolute inset-0 flex items-center justify-center w-full h-full opacity-50 dark:opacity-100 mix-blend-overlay"
-          style={{ backgroundImage: `url("/images/experiments/noise.svg")` }}
-        />
-      </div>
-      <div className="relative p-8 md:p-20 w-full max-w-[800px]">
-        <div className="mb-10 ">
-          <h2 className="text-4xl font-black text-transparent bg-gradient-to-r from-pink-400 to-indigo-500 dark:from-pink-400/80 dark:to-indigo-500/50 bg-clip-text mix-blend-overlay md:text-6xl drop-shadow-lg">
-            Smol Experiments
-          </h2>
-          <p className="text-xl text-black/40 mix-blend-overlay drop-shadow-lg dark:text-white/40">
-            UI and Code explorations by{" "}
+        >
+          <div
+            className={classnames(
+              "w-full h-full duration-[2000ms] rounded-[100px] transform-gpu bg-gradient-to-r from-purple-200 to-pink-200 dark:from-purple-500/50 dark:to-pink-600/50",
+              { "scale-[1.2] blur-[200px]": animate },
+              {
+                "scale-[3] blur-[250px]": !animate,
+              }
+            )}
+          />
+        </div>
+        <div className="relative p-8 md:p-20 w-full max-w-[800px]">
+          <div className="mb-10 ">
             <Link href="/">
-              <a className="underline">Rishi</a>
-            </Link>
-            <span className="px-1">/</span>
-            <Link href="https://twitter.com/thelifeofrishi">
-              <a target="_blank" className="underline">
-                @thelifeofrishi
+              <a
+                className={classnames(
+                  "py-1 rounded-lg text-pink-700/80 dark:text-pink-300/70 shadow-gray-200/10 flex items-center"
+                )}
+              >
+                <span className="w-5 h-5 rotate-180">{ArrowIcon}</span>
+                Back to site
               </a>
             </Link>
-          </p>
-        </div>
-        <div className="relative">
-          {LIST.map((item) => {
-            return (
-              <Link key={item.url} href={item.url}>
-                <a
-                  className={classnames(
-                    "flex flex-row py-4 duration-200 rounded-xl group ",
-                    { "pointer-events-none opacity-30": item.disabled },
-                    { "opacity-80 hover:opacity-100": !item.disabled }
-                  )}
-                >
-                  <div className="hidden md:flex translate-x-[-20px] mt-px group-hover:translate-x-0 group-hover:opacity-100 pt-px duration-[250ms] opacity-0 ease-[cubic-bezier(.75,-0.5,0,1.75)]">
-                    <div className="w-6 h-6">{ArrowIcon}</div>
-                  </div>
-                  <div className="flex flex-col pl-1">
-                    <h2 className="flex items-center text-xl font-semibold text-gray-800 dark:text-white">
-                      <span className="border-b border-gray-500 dark:border-gray-200">{item.title}</span>
-                    </h2>
-                    <p className="text-lg opacity-50 dark:opacity-60">
-                      {item.description}
-                    </p>
-                  </div>
+            <h2 className="text-4xl font-black text-transparent bg-gradient-to-r from-pink-400 to-indigo-500 dark:from-pink-400/80 dark:to-indigo-500/50 bg-clip-text md:text-6xl drop-shadow-lg">
+              Experiments
+            </h2>
+            <p className="text-xl text-black/40 drop-shadow-lg dark:text-white/40">
+              UI and Code explorations by{" "}
+              <Link href="https://twitter.com/thelifeofrishi">
+                <a target="_blank">
+                  @<span className="underline">thelifeofrishi</span>
                 </a>
               </Link>
-            );
-          })}
+            </p>
+          </div>
+          <div className="relative">
+            {LIST.map((item) => {
+              return (
+                <Link key={item.url} href={item.url}>
+                  <a
+                    className={classnames(
+                      "flex flex-row py-4 duration-200 rounded-xl group ",
+                      { "pointer-events-none opacity-30": item.disabled },
+                      { "opacity-80 hover:opacity-100": !item.disabled }
+                    )}
+                  >
+                    <div className="hidden md:flex translate-x-[-20px] mt-px group-hover:translate-x-0 group-hover:opacity-100 pt-px duration-[250ms] opacity-0 ease-[cubic-bezier(.75,-0.5,0,1.75)]">
+                      <div className="w-6 h-6">{ArrowIcon}</div>
+                    </div>
+                    <div className="flex flex-col pl-1">
+                      <h2 className="flex items-center text-xl font-semibold text-gray-800 dark:text-white">
+                        <span className="border-b border-gray-500 dark:border-gray-200">
+                          {item.title}
+                        </span>
+                      </h2>
+                      <p className="text-lg opacity-50 dark:opacity-60">
+                        {item.description}
+                      </p>
+                    </div>
+                  </a>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- pages/experiments/splash-screen.js
+++ pages/experiments/splash-screen.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import classnames from "classnames";
 import {SpinnerIcon, ArrowIcon} from "lib/icons"
+import {NextSeo} from "next-seo"
+import Link from "next/link"
 
 export default function Splash() {
   const [loaded, setLoaded] = useState(false);
@@ -21,70 +23,98 @@ export default function Splash() {
     return (
       <div
         className={classnames(
-          "flex flex-col space-y-3",
+          "flex items-center justify-center min-h-screen max-w-[550px] mx-auto py-20 antialiased px-5",
           { "scale-0 opacity-0 duration-[200ms]": !showContent },
           { "scale-100 opacity-100": showContent }
         )}
       >
-        <button
-          className={classnames(
-            "px-4 py-1 bg-white border border-gray-500 rounded-lg shadow-lg dark:bg-gray-900 dark:border-gray-700/70 dark:text-gray-200 shadow-gray-200/10"
-          )}
-          onClick={() => window.location.reload()}
-        >
-          Preview again
-        </button>
-        <button
-          className={classnames(
-            "px-4 py-1 rounded-lg text-gray-500 dark:text-gray-500 shadow-gray-200/10 flex items-center"
-          )}
-          onClick={() => (window.location.href = "/experiments")}
-        >
-          <span className="w-5 h-5 rotate-180">{ArrowIcon}</span>
-          Back to experiments
-        </button>
+        <div>
+          <Link href="/experiments">
+            <a
+              className={classnames(
+                "py-1 rounded-lg text-gray-500 dark:text-gray-500 shadow-gray-200/10 flex items-center"
+              )}
+            >
+              <span className="w-5 h-5 rotate-180">{ArrowIcon}</span>
+              View all experiments
+            </a>
+          </Link>
+          <div>
+            <h1 className="text-4xl font-black">Splash Screen</h1>
+            <p className="mt-10 text-xl opacity-50">
+              A simple splash screen implementation for Web apps. It uses
+              Tailwind CSS and React's state for animations.
+            </p>
+            <p className="mt-10 text-xl opacity-50">
+              I plan to eventually release it as a separate NPM package which
+              can just be imported and ready to use. Till then it'll go under
+              interations until it's right.
+            </p>
+          </div>
+          <div className="mt-8">
+            <button
+              className={classnames(
+                "px-4 py-1 bg-white border border-gray-500 rounded-lg shadow-lg dark:bg-gray-900 dark:border-gray-700/70 dark:text-gray-200 shadow-gray-200/10 inline-flex"
+              )}
+              onClick={() => window.location.reload()}
+            >
+              Preview again
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-200 dark:bg-[#222]">
+    <>
+      <NextSeo
+        title="Splash Screen - Rishi Mohan"
+        description="An open-source splash screen implementation for web apps made in Tailwind CSS and React"
+        openGraph={{
+          site_name: "Hi, I'm Rishi Mohan!",
+          title: "Splash Screen Implementation on Web - Rishi Mohan",
+          description: "An open-source splash screen implementation for web apps made in Tailwind CSS and React.",
+        }}
+      />
       {!showContent ? (
-        <div
-          className={classnames(
-            "font-black text-white text-4xl transform-gpu flex items-center justify-center flex-col"
-          )}
-        >
+        <div className="absolute inset-0 flex items-center justify-center bg-gray-50 dark:bg-[#111]">
           <div
             className={classnames(
-              "ease-[cubic-bezier(.75,-0.5,0,1.75)] duration-[800ms]",
-              {
-                "scale-100": loaded,
-              },
-              { "scale-0 opacity-0 blur-0": !loaded },
-              {
-                "scale-[2] opacity-0": isLoading,
-              }
+              "font-black text-white text-4xl transform-gpu flex items-center justify-center flex-col"
             )}
           >
-            <img
-              src="/images/experiments/kizie-icon.png"
-              className="w-[64px] h-[64px] shadow-lg rounded-2xl"
-            />
-          </div>
-          <div
-            className={classnames(
-              "flex justify-center w-5 h-5 mt-5 text-center",
-              { "opacity-0": isLoading }
-            )}
-          >
-            {SpinnerIcon}
+            <div
+              className={classnames(
+                "ease-[cubic-bezier(.75,-0.5,0,1.75)] duration-[800ms]",
+                {
+                  "scale-100": loaded,
+                },
+                { "scale-0 opacity-0 blur-0": !loaded },
+                {
+                  "scale-[2] opacity-0": isLoading,
+                }
+              )}
+            >
+              <img
+                src="/images/experiments/kizie-icon.png"
+                className="w-[64px] h-[64px] shadow-lg rounded-2xl"
+              />
+            </div>
+            <div
+              className={classnames(
+                "flex justify-center w-5 h-5 mt-5 text-center",
+                { "opacity-0": isLoading }
+              )}
+            >
+              {SpinnerIcon}
+            </div>
           </div>
         </div>
       ) : (
         ""
       )}
       <RenderContent />
-    </div>
+    </>
   );
 }
