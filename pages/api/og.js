2ca33e0d2b0640a32ae67d50e9c12994ad5de4ac
Rishi <iamrishi.ms@gmail.com>
2025-03-31 10:01:32 +0200

    add: use Orshot for Ogs

--- pages/api/og.js
+++ pages/api/og.js
@@ -4,16 +4,37 @@ const pikaSdk = new PikaSdk(process.env.PIKA_API_KEY);
 export default async function handler(req, res) {
   const { title, description } = req.query;
 
-  const response = await pikaSdk.generateImageFromTemplate(
-    "open-graph-image-1",
-    {
-      title: title,
-      description: description,
-      textColor: "#fff",
-      backgroundImageUrl: "https://rishimohan.me/images/site/meta-bg.png",
+  // const response = await pikaSdk.generateImageFromTemplate(
+  //   "open-graph-image-1",
+  //   {
+  //     title: title,
+  //     description: description,
+  //     textColor: "#fff",
+  //     backgroundImageUrl: "https://rishimohan.me/images/site/meta-bg.png",
+  //   },
+  //   "binary"
+  // );
+
+  const response = await fetch("https://api.orshot.com/v1/generate/images", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${process.env.ORSHOT_API_KEY}`,
     },
-    "binary"
-  );
+    body: JSON.stringify({
+      templateId: "open-graph-image-1",
+      response: {
+        format: "png",
+        type: "binary",
+      },
+      modifications: {
+        title: title,
+        description: description,
+        textColor: "#fff",
+        backgroundImageUrl: "https://rishimohan.me/images/site/meta-bg.png",
+      },
+    }),
+  }).then((res) => res.blob());
 
   const arrayBuffer = await response.arrayBuffer();
   const image = Buffer.from(arrayBuffer);
